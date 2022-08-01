1dcab36f6fa51d2bccc00c9002f1200f58182433
light <light@hellotalk.net>
2022-08-01 19:17:18 +0800

    *

--- goim-tmp/cmd/comet/main.go
+++ goim-tmp/cmd/comet/main.go
@@ -1,8 +1,22 @@
-package comet
+package main
 
-import "flag"
+import (
+	"flag"
+	"goim-study/goim-tmp/internal/comet/conf"
+	"math/rand"
+	"runtime"
+	"time"
+)
 
 func main() {
 	flag.Parse()
+	if err := conf.Init(); err != nil {
+		panic(err)
+	}
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	// 初始化server
 
 }
